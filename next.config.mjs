d4e806d9f12c07b52ac095ef29f8a7653afeee91
Howard Wu <howard@Howards-MacBook-Air.local>
2026-04-06 23:36:34 +0800

    fix: use static export for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   transpilePackages: ['remark-gfm', 'remark', 'unified'],
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
